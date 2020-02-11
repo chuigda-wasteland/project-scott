28b83f10668b3a3e86523cb1b5a4d08fc75c25c4
ICEY <icey@icey.tech>
2020-02-11 20:21:06 +0800

    maybe we should rewrite with Go or C

--- minibench/minilsm.py
+++ minibench/minilsm.py
@@ -3,13 +3,14 @@
 from typing import Dict
 from collections import OrderedDict
 
-class LSMBlockCache:
-    def __init__(self, max_size: int):
-        self.max_size = max_size
+
+class LSMCacheManager:
+    def __init__(self, max_count: int):
+        self.max_count = max_count
         self.cache_blocks: OrderedDict[str, Dict[str, str]] = OrderedDict()
 
     def allocate_cache(self, block_file_name: str) -> Dict[str, str]:
-        if len(self.cahce_blocks) >= self.cache_size_max:
+        if len(self.cache_blocks) >= self.max_count:
             self.cache_blocks.popitem()
         ret = dict()
         self.cache_blocks[block_file_name] = ret
@@ -25,8 +26,18 @@ class LSMBlockCache:
         if block_file_name in self.cache_blocks:
             del self.cache_blocks[block_file_name]
 
+
+class LSMBlockIter:
+    def __init__(self, block_file_name: str):
+        self.block_file_name = block_file_name
+        self.fd = open(self.block_file_name, "r")
+
+    def at_end(self) -> bool:
+        self.fd.readline()
+
+
 class LSMBlock:
-    def __init__(self, block_file_name: str, low: str, high: str, block_cache: LSMBlockCache):
+    def __init__(self, block_file_name: str, low: str, high: str, block_cache: LSMCacheManager):
         self.block_file_name = block_file_name
         self.low = low
         self.high = high
@@ -37,9 +48,9 @@ class LSMBlock:
 
     def get_high(self) -> str:
         return self.high
-        
+
     def get(self, key: str) -> str:
-        if key < self.low or key > self.get_high:
+        if key < self.get_low() or key > self.get_high():
             return None
         cache = self.block_cache.get_cache(self.block_file_name)
         if cache is not None:
@@ -52,7 +63,7 @@ class LSMBlock:
                     new_cache[kvpair[0]] = kvpair[1]
             return new_cache.get(key)
 
-    def interleaves_with(self, another: LSMBlock) -> bool:
+    def interleaves_with(self, another: "LSMBlock") -> bool:
         if self.get_low() <= another.get_high() and self.get_high() >= another.get_low():
             return True
         elif another.get_low() <= self.get_high() and another.get_high() >= self.get_low():
@@ -60,11 +71,13 @@ class LSMBlock:
         else:
             return False
 
+
 def insert_unique(l: list, a: any):
     for item in l:
         if item == a:
             return
     l.push(a)
+
 
 class LSMLevel:
     def __init__(self, level: int, manifest_file_name: str):
@@ -76,12 +89,12 @@ class LSMLevel:
         ret = None
         for block in self.blocks:
             ret = block.get(key)
-            if ret is not None and self.level is not 1:
+            if ret is not None and self.level != 1:
                 break
         return ret
-    
+
     def merge_blocks(self, blocks: [LSMBlock]) -> bool:
-        if self.level is 1:
+        if self.level == 1:
             self.blocks += blocks
         else:
             blocks_to_merge = []
@@ -91,19 +104,21 @@ class LSMLevel:
                         insert_unique(blocks_to_merge, incoming_block)
             pass
 
+
 class LSMTree:
     def __init__(self, folder_name: str, memtable_size_thres: int):
         self.folder_name = folder_name
         self.mem_table: Dict[str, str] = dict()
+        self.memtable_size_thres = memtable_size_thres
         self.levels: [LSMLevel] = []
 
     def put(self, key: str, value: str) -> bool:
         self.mem_table[key] = value
-        maybe_compaction()
+        self.maybe_compaction()
 
     def delete(self, key: str) -> bool:
-        self.mem_table[key] = None
-        maybe_compaction()
+        del self.mem_table[key]
+        self.maybe_compaction()
 
     def maybe_compaction(self):
         if len(self.mem_table) >= self.memtable_size_thres:
